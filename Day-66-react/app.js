e19e64d765e8824025554887bbf5c9988a8d848b
alok84087@gmail.com <alok84087@gmail.com>
2025-12-19 21:47:03 +0530

    Day-66

---
+++ Day-66-react/app.js
@@ -0,0 +1,1 @@
+// import alok from './index'
---
+++ Day-66-react/index.js
@@ -0,0 +1,13 @@
+// const arr = [1, 2, 3, 4, 5];
+// const arr1 = [...arr];
+// console.log(arr1);
+
+
+// const user = {  name: "John",
+//   age: 30,
+//   city: "New York"
+// };
+// const user1 = { ...user, country: "USA" };
+// console.log(user1);
+// console.log(alok);
+// export default "alok";
